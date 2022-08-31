a255796927dc5db40c7719c5fcc01654dcc14f17
Bruno Silva <bruno.cassiobdc@gmail.com>
2022-08-31 19:49:00 -0300

    Projection of author data with DTO created

--- src/main/java/com/bruncts/workshopmongo/config/Instantiation.java
+++ src/main/java/com/bruncts/workshopmongo/config/Instantiation.java
@@ -10,34 +10,38 @@ import org.springframework.context.annotation.Configuration;
 
 import com.bruncts.workshopmongo.domain.Post;
 import com.bruncts.workshopmongo.domain.User;
+import com.bruncts.workshopmongo.dto.AuthorDTO;
 import com.bruncts.workshopmongo.repositories.PostRepository;
 import com.bruncts.workshopmongo.repositories.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post pos1 = new Post(null,sdf.parse("30/08/2022"),"Let's go to Brazil!","I gonna travel to Brazil!",maria);
-		Post pos2 = new Post(null,sdf.parse("31/08/2022"),"Good Morning Brazil","I waked up in other country today.",maria);
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post pos1 = new Post(null, sdf.parse("30/08/2022"), "Let's go to Brazil!", "I gonna travel to Brazil!",
+				new AuthorDTO(maria));
+		Post pos2 = new Post(null, sdf.parse("31/08/2022"), "Good Morning Brazil", "I waked up in other country today.",
+				new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(pos1, pos2));
 	}
 
--- src/main/java/com/bruncts/workshopmongo/domain/Post.java
+++ src/main/java/com/bruncts/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.bruncts.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -16,12 +18,12 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User autor;
+	private AuthorDTO autor;
 
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User autor) {
+	public Post(String id, Date date, String title, String body, AuthorDTO autor) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -62,11 +64,11 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAutor() {
+	public AuthorDTO getAutor() {
 		return autor;
 	}
 
-	public void setAutor(User autor) {
+	public void setAutor(AuthorDTO autor) {
 		this.autor = autor;
 	}
 
